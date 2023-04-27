c27faeb5a1a3480d48c4a4811eeab6af9a2c5d3d
marie-claire-yui <marieclairekasai@gmail.com>
2023-04-27 10:47:31 +0200

    a

---
+++ lesson_codecademy_nested-loops/script.js
@@ -0,0 +1,14 @@
+//  One use for a nested for loop is to compare the elements in two arrays. For each round of the outer for loop, the inner for loop will run completely.
+
+console.log("hello world");
+
+// const myArray = [6, 19, 20];
+// const yourArray = [19, 81, 2];
+
+// for (let i = 0; i < myArray.length; i++) {
+//   for (let j = 0; j < yourArray.length; j++) {
+//     if (myArray[i] === yourArray[j]) {
+//       console.log('Both arrays have the number: ' + yourArray[j]);
+//     }
+//   }
+// }
